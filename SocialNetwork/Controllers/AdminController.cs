57bafe571794412fbc97ba24f5c0eca54a2ce28c
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-03-20 21:27:47 +0700

    load data to admin view

--- SocialNetwork/Controllers/AdminController.cs
+++ SocialNetwork/Controllers/AdminController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+using X.PagedList;
 
 namespace SocialNetwork.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        SocialNetworkDbContext context = new SocialNetworkDbContext();
+        public IActionResult Index(int? page)
         {
-            return View();
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSise = 3;
+            var listAccount = context.Accounts.AsNoTracking().OrderBy(x => x.AccountId).ToList();
+            PagedList<Account> list = new PagedList<Account>(listAccount, pageNumber, pageSise);
+            return View(list);
         }
     }
 }
