6ffa94b52aafa3b96722d8eddc677d44ec06f224
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-03-21 00:19:58 +0700

    add feature ban account

--- SocialNetwork/Controllers/AdminController.cs
+++ SocialNetwork/Controllers/AdminController.cs
@@ -7,14 +7,44 @@ namespace SocialNetwork.Controllers
 {
     public class AdminController : Controller
     {
+
         SocialNetworkDbContext context = new SocialNetworkDbContext();
         public IActionResult Index(int? page)
         {
-            int pageNumber = page == null || page < 1 ? 1 : page.Value;
-            int pageSise = 3;
-            var listAccount = context.Accounts.AsNoTracking().OrderBy(x => x.AccountId).ToList();
-            PagedList<Account> list = new PagedList<Account>(listAccount, pageNumber, pageSise);
-            return View(list);
+            CurrentAccount.initSession(1);
+            
+
+            if(CurrentAccount.account.IsAdmin == true)
+            {
+                int pageNumber = page == null || page < 1 ? 1 : page.Value;
+                int pageSise = 3;
+                var listAccount = context.Accounts.AsNoTracking().OrderBy(x => x.AccountId).ToList();
+                PagedList<Account> list = new PagedList<Account>(listAccount, pageNumber, pageSise);
+                return View(list);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult BanAccount(int? accountId)
+        {
+
+            if (CurrentAccount.account.IsAdmin == true)
+            {
+                Account accountBan = context.Accounts.SingleOrDefault(x => x.AccountId == accountId);
+                if (accountBan != null && accountBan.IsAdmin == false)
+                {
+                    accountBan.IsBanned = !accountBan.IsBanned;
+                    context.Entry(accountBan).State = EntityState.Modified;
+                    context.SaveChanges();
+                    TempData["Message"] = "Changes has been saved ";
+                    return RedirectToAction("Index");
+                }
+                TempData["Message"] = "Changes hasn't been saved !!!";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
