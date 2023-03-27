04f528f783a966953764815513c9827388031882
nokiddig <sy2000dn0@gmail.com>
2023-03-27 13:37:22 +0700

    add search model

---
+++ SocialNetwork/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SocialNetwork.Models;
+
+namespace SocialNetwork.ViewModels
+{
+    public class SearchResultViewModel
+    {
+        public List<Account> lstAccount { get; set; }
+        public List<Post> lstPost { get; set; }
+
+        public SearchResultViewModel(List<Account> lstAccount, List<Post> lstPost)
+        {
+            this.lstAccount = lstAccount;
+            this.lstPost = lstPost;
+        }
+    }
+}
