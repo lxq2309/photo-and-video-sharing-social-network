cd87780ceed754129a2cad0229d2d23933566f03
nokiddig <sy2000dn0@gmail.com>
2023-03-29 20:53:37 +0700

    add sessionViewModels

---
+++ SocialNetwork/ViewModels/ChatSessionMessagesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SocialNetwork.Models;
+
+namespace SocialNetwork.ViewModels
+{
+    public class ChatSessionMessagesViewModel
+    {
+        SocialNetworkDbContext _context = new SocialNetworkDbContext();
+        public List<KeyValuePair <ChatSession, Account>> listChatSessitonAccount { get; set; }
+        public List<Message> listMessage { get; set; }
+        public Account partner { get;set; }
+        public int chatID { get; set; }
+
+        public ChatSessionMessagesViewModel(List<KeyValuePair<ChatSession, Account>> sessions, List<Message> messages, Account account)
+        {
+            listChatSessitonAccount = sessions;
+            listMessage = messages;
+            partner = account;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
